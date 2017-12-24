b07ffb9ea0d809e8ca414aabe87c00f6aee5172c
johnblakey <blakeyjo@oregonstate.edu>
2017-12-24 16:59:01 -0700

    Displaying json property in card

--- river-level-client/src/App.js
+++ river-level-client/src/App.js
@@ -4,7 +4,27 @@ import GetRivers from './GetRivers';
 import RiverCard from './RiverCard';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = { json: '' };
+    }
+    componentDidMount() {
+        fetch('http://riverlevel.duckdns.org:2000/api/rivers')
+            .then(results => {
+                return results.json();
+            }).then(data => {
+            this.setState({ json: data });
+            console.log(this.state.json[0].siteName);
+        })
+    }
+
     render() {
+        console.log(this.state.json);
+        let siteName = "Empty";
+        if (this.state.json) {
+            siteName = this.state.json[0].siteName;
+            console.log("siteName is:", siteName);
+        }
         return (
             <div className="App">
                 <header className="App-header">
@@ -13,7 +33,7 @@ class App extends Component {
                 <p className="App-intro">
                     <GetRivers />
                 </p>
-                <RiverCard name="Name given by prop"/>
+                <RiverCard name={siteName}/>
                 <br/>
                 <br/>
                 <h5>Lose uncertainty. Not gear.</h5>
