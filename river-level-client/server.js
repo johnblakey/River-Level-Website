a01ea3668b683da479bea15b5110ef58133af022
johnblakey <blakeyjo@oregonstate.edu>
2017-12-24 17:53:03 -0700

    Added test static server

---
+++ river-level-client/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+let port = "4000";
+const server = app.listen(port, "0.0.0.0", () => {
+    var {address, port} = server.address();
+    console.log('Express react app running at http://${address}:${port}');
+});
